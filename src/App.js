91e470038c0c4fe35bf3def7d34dba6e3c53aad2
dpavlichenko <danylo.pavlichenko@sectigo.com>
2021-10-30 19:11:21 +0300

    Added postList, postItem, input, button components. Used useState and useRef hooks. Added styles to the components using modules

--- src/App.js
+++ src/App.js
@@ -1,29 +1,58 @@
 import React, {useState} from "react";
+import './styles/app.css';
+import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
 
 function App() {
 
-  const [likes, setLikes] = useState(0);
-  const [value, setValue] = useState('Input Value');
+    const [posts, setPosts] = useState([
+        {id: 1, title: 'Javascript', body: 'Description'},
+        {id: 2, title: 'Java', body: 'Description'},
+        {id: 3, title: 'C++', body: 'Description'}
+    ])
 
-  function increment() {
-    setLikes(likes + 1);
-  }
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
 
-  function decrement() {
-    setLikes(likes - 1);
-  }
+    // Using this hook we can directly get access to the element from DOM.
+    // This hook must be passed to the attribute "ref" inside the element to be accessed
+    // const bodyInputRef = useRef();
+
+    const addNewPost = (e) => {
+        e.preventDefault();
+        const newPost = {
+            id: Date.now(),
+            title,
+            body
+        }
+        setPosts([...posts, newPost]);
+        //?????
+        setTitle(' ');
+        setBody(' ');
+    }
 
   return (
     <div className="App">
-      <h1>{likes}</h1>
-      <h1>{value}</h1>
-      <input
-          type="text"
-          value={value}
-          onChange={event => setValue(event.target.value)}
-      />
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
+        <form>
+
+            <MyInput
+                type="text"
+                placeholder="Title"
+                onChange={event => setTitle(event.target.value)}/>
+            <MyInput
+                type="text"
+                placeholder="Description"
+                onChange={event => setBody(event.target.value)}/>
+
+            {/*Unruled element - НЕУПРАВЛЯЕМЫЙ*/}
+            {/*<MyInput*/}
+            {/*    ref={bodyInputRef}*/}
+            {/*    type="text"*/}
+            {/*    placeholder="Description"/>*/}
+            <MyButton onClick={addNewPost}>Add post</MyButton>
+        </form>
+        <PostList posts={posts}/>
     </div >
   );
 }
