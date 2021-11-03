6363f56d4f9a4c5fb3f55cc9531f172c4f27d16b
DaniilOleghovich <danya.pavlichenko.1997@mail.ru>
2021-11-03 23:48:05 +0200

    Added animation for delete and crete posts. Also decompose logic - create custom hooks that using standard hooks like a useMemo and do sorting and filtering of posts and return array of sorted or filtered posts

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PostItem from "./PostItem";
+import {CSSTransition, TransitionGroup} from "react-transition-group";
 
 const PostList = ({remove, posts}) => {
     if (!posts.length) {
@@ -15,9 +16,16 @@ const PostList = ({remove, posts}) => {
                 List of Posts
             </h1>
 
-            {posts.map((post, index) =>
-                <PostItem remove={remove} number={index + 1} post={post} key={post.id}/>
-            )}
+            <TransitionGroup>
+                {posts.map((post, index) =>
+                    <CSSTransition
+                        key={post.id}
+                        timeout={500}
+                        classNames="post">
+                        <PostItem remove={remove} number={index + 1} post={post}/>
+                    </CSSTransition>
+                )}
+            </TransitionGroup>
         </div>
     );
 };
